729fa466e2d11c46dff1af03e05ff2f75aefb892
Don Strong <dstrong230@gmail.com>
2022-07-21 20:22:52 -0400

    Created Entry object, added processLine()

---
+++ Entry.java
@@ -0,0 +1,47 @@
+public class Entry {
+
+    private String first;
+    private String last;
+    private String address;
+    private String city;
+    private String state;
+    private int age;
+
+    
+    public Entry(String first, String last, String address, String city, String state, int age) {
+        this.first = first;
+        this.last = last;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.age = age;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void printToString() {
+        System.out.printf("%s - %s - %s - %s - %s - %d\n", first, last, address, city, state, age);
+    }
+}
--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException{
@@ -11,14 +14,35 @@ public class Main {
     }
 
     private static void getInput() throws FileNotFoundException, IOException{
+        ArrayList<String> list = new ArrayList<String>();
+        String s;
         try (Scanner reader = new Scanner(new File("./input.txt"))) {
             while(reader.hasNextLine()) {
-                System.out.println(reader.nextLine());
+                // String[] tokens = format(reader.nextLine()).split("\"");
+                // System.out.print(tokens.length + ": ");
+                // for (String i : tokens) {
+                //     System.out.print(i + "--");
+                // }
+                // System.out.print("\n");
+                Entry e = processLine(format(reader.nextLine()));
+                e.printToString();
             }
         } catch (FileNotFoundException ex) {
             System.out.println(ex);
             System.exit(-1);
         }
+    }
+
+    private static Entry processLine(String str) {
+        String[] arr = str.split("\"");
+        
+        return new Entry(arr[1].trim(), arr[2].trim(), arr[3].trim(), arr[4].trim(), arr[5].trim(), Integer.parseInt(arr[6].trim()));
+    }
+
+    private static String format(String s) {
+        s = s.replaceAll("[,.]", "");
+        s = s.replaceAll("[\"][\"]", "\"");
+        return s;
     }
 
 
